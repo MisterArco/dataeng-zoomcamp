159137b130441c894d4710a7db166b7e41c2464d
John Paul Basilio <jpbasilio26@gmail.com>
2024-02-27 07:57:42 +0000

    Updates dbt

--- 04-analytics-engineering/models/core/fact_fhv_trips.sql
+++ 04-analytics-engineering/models/core/fact_fhv_trips.sql
@@ -5,20 +5,26 @@
 }}
 
 with fhv_tripdata as (
-    select * from {{ ref('stg_fhv_tripdata') }}
+    select *,
+        'FHV' as service_type
+    from {{ ref('stg_fhv_tripdata') }}
 ), 
 dim_zones as (
     select * from {{ ref('dim_zones') }}
     where borough != 'Unknown'
 )
 select 
+    fhv_tripdata.service_type,
     pickup_zone.borough as pickup_borough, 
     pickup_zone.zone as pickup_zone, 
     dropoff_zone.borough as dropoff_borough, 
-    dropoff_zone.zone as dropoff_zone  
+    dropoff_zone.zone as dropoff_zone,  
+    fhv_tripdata.pickup_datetime AS pickup_datetime, 
+    fhv_tripdata.dropOff_datetime AS dropoff_datetime
 from fhv_tripdata
 inner join dim_zones as pickup_zone
 on fhv_tripdata.PUlocationID = pickup_zone.locationid
 inner join dim_zones as dropoff_zone
 on fhv_tripdata.DOlocationID = dropoff_zone.locationid
+
 
